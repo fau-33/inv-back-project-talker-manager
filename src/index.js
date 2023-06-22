0258ceb1a4f068a62d43bb30bf5f43baf333ece8
Flávio Félix <fau_felix30@hotmail.com>
2023-06-22 15:04:50 -0300

    Mandando de novo o req 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { readTalker } = require('./talkerFuntions');
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
