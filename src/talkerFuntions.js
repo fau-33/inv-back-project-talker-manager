15f8fd4f05ca859092d3f2294eb0ce9b35cf0626
Flávio Félix <fau_felix30@hotmail.com>
2023-06-22 15:20:42 -0300

    REQ 1 enviando novamente

--- src/talkerFuntions.js
+++ src/talkerFuntions.js
@@ -1,14 +1,12 @@
 const fs = require('fs').promises;
 const { join } = require('path');
 
-
 const path = join(__dirname, 'talker.json');
 
 const readTalker = async () => {
     const talkers = await fs.readFile(path);
     return JSON.parse(talkers);
 };
-
 
 module.exports = {
     readTalker,
