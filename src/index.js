9618fd609b1f1f67e155d47d0c4fdfbdd58754d1
Flávio Félix <fau_felix30@hotmail.com>
2023-06-22 14:58:19 -0300

    Feito o req 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { readTalker } = require('./talkerFuntions');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,13 @@ const PORT = process.env.PORT || '3001';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// Req 1
+app.get('/talker', async (_req, res) => {
+  const talkers = await readTalker();
+  if (talkers.length === 0) return res.status(200).json([]);
+  res.status(200).json(talkers);
 });
 
 app.listen(PORT, () => {
